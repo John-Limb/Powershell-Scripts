0007b0c4c4be6de2e46aa6adf8b1231afaac828e
John Limb <56315426+John-Limb@users.noreply.github.com>
2023-10-31 16:06:28 +0000

    Initial Upload

---
+++ DeleteEmptyFolders.ps1
@@ -0,0 +1,9 @@
+﻿$Path = ""
+#Now we delete the empty folders in base directory defined in params
+param(
+    [Parameter(Mandatory=$True)]
+    [string] $Path
+)
+Get-ChildItem -Path $Path -Force -Recurse -Directory |
+    Where-Object { (Get-ChildItem -Path $_.FullName -Recurse -File -EA SilentlyContinue |
+        Measure-Object).Count -eq 0 } | Remove-Item -Force -Recurse
---
+++ Get-empty-folders.ps1
@@ -0,0 +1,18 @@
+﻿$logfile = "C:\log\proc_$env:computername-emptyfolderclear.log"
+function WriteLog
+{
+Param ([string]$LogString)
+$stamp = (Get-Date).ToString("yyyy/MM/dd HH:mm:ss")
+$LogMessage = "$stamp $LogString"
+Add-Content $logfile -Value $LogMessage
+}
+
+#Params
+$Path = ""
+
+#Find All Empty folders and write them to log file
+Get-ChildItem -Path $Path -Directory -Recurse | ForEach-Object {
+    if (!(Get-ChildItem -Path $_.FullName)) {
+    Writelog $_.FullName
+    }
+}
